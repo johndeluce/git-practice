1ab754f86f41fea78a04689453efa3dfd7ffd759
John <johnrdeluce@gmail.com>
2026-05-04 13:37:32 -0400

    Make multiplication function

--- calculator.py
+++ calculator.py
@@ -6,9 +6,11 @@ def add(a,b):
 def subtract(a,b):
     return a - b
 
-
 if __name__ == "__main__":
     print("Calculator loaded!")
     print(f"5 + 3 = {add(5,3)}")
     print(f"10 - 4 = {subtract(10,4)}")
+
+def multiply(a,b):
+    return a * b
 
