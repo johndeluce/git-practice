bdf93d6f2b97ce5f6c1d1afd358015355df3c7b9
John DeLuce <144717942+johndeluce@users.noreply.github.com>
2026-05-05 09:28:19 -0400

    Update hello.py from GitHub

--- hello.py
+++ hello.py
@@ -1,4 +1,2 @@
-print("Hello from both GitHub and local computer!")
-print("This version combines both edits")
-print("Local version: edited locally")
-print("GitHub version: edited on GitHub")
+print("Greetings from GitHub!")
+print("This version was edited on the web")
