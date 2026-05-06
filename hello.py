a559fe9212da14acec882b5b511f4a9a56f44f0e
John DeLuce <johnrdeluce@gmail.com>
2026-05-06 13:54:46 -0400

    Change formatting to allow merge

--- hello.py
+++ hello.py
@@ -1,3 +1,5 @@
+"""This is a simple Python script that prints a message to the console."""
+
 print("Hello from VS Code and GitHub!")
 print("This version was edited in both places")
 print("conflict resolved successfully")
